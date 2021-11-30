f75f706b3fbf8b319831738ca00943972b69cd8d
ptp-phamphong <74300269+ptp-phamphong@users.noreply.github.com>
2021-11-30 15:15:56 +0700

    Pull về tính năng login, register

---
+++ SportShop/src/main/java/com/aptech/Controller/User/CustomerController.java
@@ -0,0 +1,15 @@
+package com.aptech.Controller.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CustomerController {
+
+	@RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+	public String loginHandeling() {
+		
+		return "abc";
+	}
+}
