31090edf1782bcdd5fc4c2604cb9eefbb8bf21c7
ptp-phamphong <74300269+ptp-phamphong@users.noreply.github.com>
2021-12-10 23:03:20 +0700

    Thêm chức năng cart
    Thêm các chức năng cơ bản của cart
     -  Thêm sản phẩm vào cart (cái này mới là thêm từ allProduct thôi chưa trong detail được)
     - Các nút + -  số lượng, cũng ajax cả đấy.

--- SportShop/src/main/java/com/aptech/Controller/User/ProductController.java
+++ SportShop/src/main/java/com/aptech/Controller/User/ProductController.java
@@ -19,6 +19,7 @@ import com.aptech.Dao.ProductDetailDao;
 import com.aptech.Model.Cart;
 import com.aptech.Model.Customer;
 import com.aptech.Model.Product;
+import com.aptech.Model.ProductDetail;
 import com.aptech.MyClass.SendingEmail;
 
 @Controller
@@ -51,50 +52,119 @@ public class ProductController {
 		return mv;
 	}
 	
+	
+	//Các hàm xử lý giỏ hàng
 	@RequestMapping(value = { "/cart" }, method = RequestMethod.GET)
-	public ModelAndView showCart(Model model) {
+	public ModelAndView showCart(Model model, HttpServletRequest request) {
 		model.addAttribute("customer", new Customer());	
+		
 		ModelAndView mv = new ModelAndView("user/cart");
-		ArrayList<Cart> cartList = new ArrayList();//TEST sau thay bang session
-		cartList.add(new Cart(1, 2, 10));
-		cartList.add(new Cart(2, 1, 15));
-		mv.addObject("cartList", cartList);
 		return mv;
 	}
 	
 	
-	//Các hàm xử lý giỏ hàng
+	
 	@ResponseBody
-	@RequestMapping(value = { "/addToCart" }, method = RequestMethod.POST)
-	public String sendEmail( HttpServletRequest request) {
+	@RequestMapping(value = { "/addItemToCart" }, method = RequestMethod.POST)
+	public String addItemToCart( HttpServletRequest request) {
 		int idProduct = Integer.parseInt(request.getParameter("idProduct"));
+		ProductDetailDao productDetailDao = new ProductDetailDao();
+		ProductDetail productToCart = new ProductDetail();
 		
 		HttpSession session = request.getSession();
 		ArrayList<Cart> curCart = new ArrayList<Cart>();
-		curCart = (ArrayList<Cart>) session.getAttribute("cart");
+		curCart = (ArrayList<Cart>) session.getAttribute("curCart");
 		
+		//Xét xem người đó có nhập số lượng vào ko
+		int quantity = 1;
+		if(request.getAttribute("quantity") != null)
+			quantity =  Integer.parseInt(request.getAttribute("quantity").toString());
+		
+		//Xét xem người dùng họ thêm sản phẩm size ở thứ tự nào
+		int numOfSize = 0;
+		if(request.getAttribute("size") != null)
+			numOfSize =  Integer.parseInt(request.getAttribute("size").toString());
+		productToCart =  productDetailDao.getByIdProduct(idProduct).get(numOfSize);
 		
 		if(curCart == null) {//Nếu trong cart chưa có gì
-			curCart = new ArrayList<Cart>();
-			
+			curCart = new ArrayList<Cart>();//Khởi tạo mới được
 			Cart newItem = new Cart();
-			ProductDao productDao = new ProductDao();
-			ProductDetailDao productDetailDao = new ProductDetailDao();
-			Product productToCart = new Product();
-			productToCart =  productDao.getByProductID(Integer.parseInt(request.getParameter("idProduct")));
-			//newItem.set
-			newItem.setQuantity(1);
-			newItem.setPrice(productDetailDao.getByIdProduct(idProduct).get(0).getPrice());
-			newItem.setProductDetail(productDetailDao.getByIdProduct(idProduct).get(0));
+			newItem.setQuantity(quantity);
+			newItem.setPrice(productToCart.getPrice());
+			newItem.setProductDetail(productToCart);
+			newItem.setProductDetailsID(productToCart.getId());
 			curCart.add(newItem);
 			session.setAttribute("curCart", curCart);
+			return "done";
 		}
-			
-			
+		
+		
+		//Nếu trong cart có gì đó rồi, thì phải xét xem món đó đã từng nằm trong cart chưa, nếu có thì chỉ tăng số lượng lên quantity thôi
+		for(int i=0;i<curCart.size();i++) {
+			if(productToCart.getId() == curCart.get(i).getProductDetailsID()) {//Nếu từng có rồi
+				curCart.get(i).setQuantity(curCart.get(i).getQuantity() + quantity);
+				session.setAttribute("curCart", curCart);
+				return "done";
+			}
+		}
+		
+		//Nếu đây là 1 món hoàn toàn mới
+		Cart newItem = new Cart();
+		newItem.setQuantity(quantity);
+		newItem.setPrice(productToCart.getPrice());
+		newItem.setProductDetail(productToCart);
+		newItem.setProductDetailsID(productToCart.getId());
+		curCart.add(newItem);
+		session.setAttribute("curCart", curCart);
+		
+		
 		return "done";
 	}
 	
 	
+	//Hàm Ajax tăng số lượng trong giỏ hàng
+	@ResponseBody
+	@RequestMapping(value = { "/ajax/increseQuantity" }, method = RequestMethod.POST)
+	public ModelAndView increseQuantity( HttpServletRequest request) {
+		int idProductDetail = Integer.parseInt(request.getParameter("idProductDetail"));
+		
+		HttpSession session = request.getSession();
+		ArrayList<Cart> curCart = new ArrayList<Cart>();
+		curCart = (ArrayList<Cart>) session.getAttribute("curCart");
+		
+		for(int i=0;i<curCart.size();i++) {
+			if(curCart.get(i).getProductDetail().getId() == idProductDetail) {
+				curCart.get(i).setQuantity(curCart.get(i).getQuantity() + 1);
+				break;
+			}
+		}
+		
+		ModelAndView mv = new ModelAndView("user/cartDetail");
+		return mv;
+	}
+	
+	
+	//Hàm ajax, giảm số lượng trong giỏ hàng
+	@ResponseBody
+	@RequestMapping(value = { "/ajax/decreaseQuantity" }, method = RequestMethod.POST)
+	public ModelAndView decreaseQuantity( HttpServletRequest request) {
+		int idProductDetail = Integer.parseInt(request.getParameter("idProductDetail"));
+		HttpSession session = request.getSession();
+		ArrayList<Cart> curCart = new ArrayList<Cart>();
+		curCart = (ArrayList<Cart>) session.getAttribute("curCart");
+		
+		for(int i=0;i<curCart.size();i++) {
+			if(curCart.get(i).getProductDetail().getId() == idProductDetail) {
+				if(curCart.get(i).getQuantity() == 1) //Nếu số lượng là 1 rồi mà còn muốn giảm thì dell cho giảm
+					break;
+				curCart.get(i).setQuantity(curCart.get(i).getQuantity() - 1);
+				break;
+			}
+		}
+		
+		ModelAndView mv = new ModelAndView("user/cartDetail");
+		return mv;
+	}
 	
 	
 	
